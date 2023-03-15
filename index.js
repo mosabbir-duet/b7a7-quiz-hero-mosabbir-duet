1c1a1ede3dcf9046881b010a7ac446eec1f1f9f4
mosabbir.duet <mosabbir.duet@gmail.com>
2023-03-16 00:44:08 +0600

    Solved to store previous result in localStorage

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ document.querySelector("#submit").addEventListener("click", () => {
   let storage = JSON.parse(localStorage.getItem("result"));
   if (storage) {
     localStorage.setItem(
-      "results",
+      "result",
       JSON.stringify([
         ...storage,
         {
@@ -126,7 +126,7 @@ document.querySelector("#submit").addEventListener("click", () => {
     );
   } else {
     localStorage.setItem(
-      "results",
+      "result",
       JSON.stringify([
         {
           marks: totalMark,
